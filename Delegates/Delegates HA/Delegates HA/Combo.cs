df0921d75490898e4244c5b398c3968d615f2901
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-01-02 15:54:01 +0200

    Delegates WPF

---
+++ Delegates/Delegates HA/Delegates HA/Combo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+class Combo
+{
+    ComboBox comboBox;
+
+    public ComboBox ComboBox { get => comboBox; set => comboBox = value; }
+
+   
+
+    Dictionary<int, Func<int, int, int>> keyValuePairs = new Dictionary<int, Func<int, int, int>>();
+
+    List<Func<int, int, int>> funcs = new List<Func<int, int, int>>()
+    {
+      
+        
+
+    };
+
+
+    public void UsingComboBox()
+    {
+        
+
+    }
+}
+
+
+
---
+++ Delegates/Delegates HA/Delegates HA/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Delegates_HA
+{
+    public partial class MainWindow : Window
+    {
+
+ 
+        Func<int, int, int> Calculation;
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public int Multiply(int x, int y)
+        {
+          return x * y;
+        }
+
+        public int Addition(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Moudulu(int x, int y)
+        {
+            return x % y;
+        }
+
+        public int Power(int x, int power)
+        {
+            int number = 0;
+            for (int i = 0; i < power; i++)
+            {
+                number += (x * x);
+            }
+
+            return number;
+        }
+
+        public int Divide(int x, int y)
+        {
+            return x / y;
+        }
+
+   
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+           
+
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int index = Combobox.SelectedIndex;
+
+            switch (index)
+            {
+
+                case 0:
+                    Calculation = Subtract;
+                    break;
+                case 1:
+                    Calculation = Multiply;
+                    break;
+                case 2:
+                    Calculation = Addition;
+                    break;
+                case 3:
+                    Calculation = Moudulu;
+                    break;
+                case 4:
+                    Calculation = Power;
+                    break;
+                case 5:
+                    Calculation = Divide;
+                    break;
+
+
+            }
+
+            int num1 = int.Parse(NumberX.Text);
+            int num2 = int.Parse(NumberY.Text);
+            Result.Content = Calculation(num1, num2).ToString();
+
+
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+
+        }
+
+        private void NumberX_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void NumberY_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}

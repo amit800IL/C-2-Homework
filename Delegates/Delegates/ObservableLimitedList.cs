5eb516902622c9e153cf1d1d725416267055660d
amit800IL <108745954+amit800IL@users.noreply.github.com>
2023-01-02 15:49:05 +0200

    Delegates Homework

---
+++ Delegates/Delegates/ObservableLimitedList.cs
@@ -0,0 +1,56 @@
+﻿class ObservableLimitedList
+{
+    private List<string> stringList = new List<string>();
+
+    public event Func<string, string> listChanged;
+
+    private CheckStringValidity predicate;
+
+
+
+    public ObservableLimitedList(CheckStringValidity validity)
+    {
+        predicate = validity;
+
+
+        listChanged += PrintChangedItem;
+
+    }
+    public void TryAdd(string inputString)
+    {
+        if (predicate != null && predicate.Invoke(inputString))
+        {
+            stringList.Add(inputString);
+            listChanged?.Invoke(inputString);
+
+        }
+
+
+    }
+
+    public void Remove(string inputString)
+    {
+        stringList.Remove(inputString);
+        listChanged?.Invoke(inputString);
+    }
+
+    public void PrintAll()
+    {
+        Console.WriteLine("This are the items that were added to the list: ");
+        foreach (string inputString in stringList)
+        {
+            Console.WriteLine(inputString);
+        }
+    }
+    public string PrintChangedItem(string inputString)
+    {
+
+        Console.WriteLine(inputString);
+
+        return inputString;
+
+    }
+
+
+}
+
---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Net.Mail;
+
+public delegate bool CheckStringValidity(string input);
+class Program
+{
+    
+    public static void Main(string[] args)
+    {
+        CheckStringValidity containsS = new CheckStringValidity(StringContainsLetterS);
+
+        ObservableLimitedList limited = new ObservableLimitedList(containsS);
+
+        string one = "Amit Kremer";
+        string two = "Ron Bandel";
+        string Three = "Roee Tals";
+        string four = "Ziv Atad";
+        string five = "LOLO";
+        string Six = "Ron Menson";
+        string Seven = "LOL";
+        string Eight = "Meli Anderson";
+        string Nine = "Poler Konsel";
+        string Ten = "Lili Pelson";
+
+        limited.TryAdd(one);
+        limited.TryAdd(two);
+        limited.TryAdd(Three);
+        limited.TryAdd(four);
+        limited.TryAdd(five);
+        limited.TryAdd(Six);
+        limited.TryAdd(Seven);
+        limited.TryAdd(Eight);
+        limited.TryAdd(Nine);
+        limited.TryAdd(Ten);
+
+        
+        limited.PrintAll();
+
+    }
+
+    public static bool StringContainsLetterS(string input)
+    {
+        if (input.Contains("S") || input.Contains("s"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    
+}
